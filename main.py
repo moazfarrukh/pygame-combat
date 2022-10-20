a4967f127c8d472b2bf7d47a9f1d2ab6ebfe3f96
moazfarrukh <maazfarrukh@gmail.com>
2022-10-20 15:12:54 +0500

    basic game setup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 
-#initialize modules
+# initialize modules
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()
@@ -10,26 +10,46 @@ WIDTH, HEIGHT = 1000, 700
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pygame Combat")
 
-# fonts/text setup
+# fonts setup
 title_font = pygame.font.SysFont('comicsans', 70)
-game_title = title_font.render("pygame combat", True, (187, 0, 0))
-
-# quit button
-color_dark = (100, 100, 100)
 smallfont = pygame.font.SysFont('Corbel', 50)
-text = smallfont.render('Quit', True, (255, 255, 255))
+
+
+# texts render
+game_title = title_font.render("pygame combat", True, (187, 0, 0))
+text = smallfont.render('Start', True, (255, 255, 255))
+quit_text = smallfont.render('Quit', True, (255, 255, 255))\
+
+
+
+def game():
+    ingame = True
+
+    while ingame:
+        WINDOW.fill((0, 0, 0))
+        for event in pygame.event.get():
+            # handle events that happen on in game screen
+            if event.type == pygame.QUIT:
+                ingame = False
+                exit()
+        pygame.display.update()
 
 
 def start():
     intro = True
 
     while intro:
-        # display text in the center
+        # display game title in the center
         WINDOW.blit(game_title, (WIDTH/2 - game_title.get_width() / 2, 40))
 
-        # display quit button
+        # display start button
         button = pygame.draw.rect(WINDOW, (100, 100, 100), [420, 315, 160, 60])
         WINDOW.blit(text, (460, 330))
+
+        # display quit button
+        button2 = pygame.draw.rect(
+            WINDOW, (100, 100, 100), [420, 415, 160, 60])
+        WINDOW.blit(quit_text, (460, 430))
 
         for event in pygame.event.get():
             # handle events that happen on intro screen
@@ -38,8 +58,15 @@ def start():
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if button.collidepoint(pygame.mouse.get_pos()):
+                    # mouseclick on  start button
+                    intro = False
+                    game()
+
+                if button2.collidepoint(pygame.mouse.get_pos()):
+                    # mouseclick on  quit button
                     intro = False
                     exit()
+
         pygame.display.update()
 
 
